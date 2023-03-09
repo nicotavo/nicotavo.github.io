38881d2e60146a374faa78b342182c26edfaa527
nicotavo <124008625+nicotavo@users.noreply.github.com>
2023-03-08 22:25:46 -0300

    actualizacion 02 CV
    Segunda actualización del Trabajo final de #Yo Programo.....

--- JS/codigo.js
+++ JS/codigo.js
@@ -1,16 +1,9 @@
-function resalta(elEvento) {
-    var evento = elEvento || window.event;
-    switch(evento.type) {
-    case 'mouseover':
-    this.style.borderColor = 'black';
-    break;
-    case 'mouseout':
-    this.style.borderColor = 'silver';
-    break;
-    }
-    }
-    window.onload = function() {
-    document.getElementById("seccion").onmouseover = resalta;
-    document.getElementById("seccion").onmouseout = resalta;
-    }
 
+function quienEs()
+{
+document.getElementById('Nombre').value ="    José Luis Pederneti";
+}
+function nada()
+{
+document.getElementById('Nombre').value ="";
+}
